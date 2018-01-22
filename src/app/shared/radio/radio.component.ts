65d407e2960ab17b38b255d34ea0939ec57f6d3f
ronaldo.moura <ronaldomoura30@hotmail.com>
2018-01-22 18:24:08 -0300

    Ajuste do radio button tela de pagamento do pedido

--- src/app/shared/radio/radio.component.ts
+++ src/app/shared/radio/radio.component.ts
@@ -25,7 +25,6 @@ export class RadioComponent implements OnInit, ControlValueAccessor {
 
   setValue(screenValue: any){
     this.value = screenValue
-    this.onChange = this.value
     this.registerOnChange(this.value)
   }
 
